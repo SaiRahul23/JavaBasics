ff1eeef23d65758881a7722a359d973526ef94d8
joelrego198 <46254780+joelrego198@users.noreply.github.com>
2019-01-02 01:22:26 +0530

    Update program1main.java

--- program1main.java
+++ program1main.java
@@ -36,3 +36,28 @@ class small implements Runnable{
 	//methods to find sum of even and odd can be added
 }
 
+class sort implements Runnable{
+	int arr[];
+	String name;
+	Thread t;
+	Scanner sc=new Scanner(System.in);
+	
+	sort(String name,int size){
+		arr=new int[size];
+		System.out.println("Enter the elements: ");
+		for(int i=0;i<size;i++) {
+			arr[i]=sc.nextInt();
+		}
+		t=new Thread(this,name);
+		System.out.println("New Thread: "+t);
+		t.start();
+	}
+	public void run() {
+		Arrays.sort(arr);
+		for(int i=0;i<arr.length;i++)
+			System.out.print(" "+arr[i]);
+	}
+	
+	//methods to find sum of even and odd can be added
+}
+
